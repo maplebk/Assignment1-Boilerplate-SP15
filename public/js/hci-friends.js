a62af7e47f010937e3ed9c19f9157a23c20599f4
Michael Garcia <mcg003@ucsd.edu>
2016-11-09 23:40:32 -0800

    add ingredient ability

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -1,46 +1,10 @@
 'use strict';
 
-// Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
-	initializePage();
-})
+function add_ingredient() {
+	var ingredient = $("#ingredient").val();
+	$("#ingredientlist").append("<li> ingredient <li>" );
 
-/*
- * Function that is called when the document is ready.
- */
-function initializePage() {
-	console.log("Javascript connected!");
-}
+	}
 
-function anagrammedName(name) {
-	// Thanks, Internet Anagram Server!
-	
-	if (name == "Doug Engelbart") {
-		return "Notable Grudge";
-	} 
-	else if (name == "Ivan Sutherland") {
-		return "Vandal Heist Run";
-	}
-	else if (name == "JCR Licklider") {
-		return "Crick Rid Jell";
-	}
-	else if (name == "Vannevar Bush") {
-		return "Ravens Van Hub";
-	}
-	else if (name == "Alan C. Kay") {
-		return "Canal Yak";
-	}
-	else if (name == "Allen Newell") {
-		return "Ellen All New";
-	}
-	else if (name == "Lucy Suchman") {
-		return "Lunacy Chums";
-	}
-	else if (name == "Grace Hopper") {
-		return "Gear Chopper";
-	}
-	else {
-		console.log(name + " not known for anagramming.");
-		return name;
-	}
-}
+$("#readytoadd").click(add_ingredient);
+
