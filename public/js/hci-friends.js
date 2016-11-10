253807261b869940b91ab92f0366cede9041b899
Michael Garcia <mcg003@ucsd.edu>
2016-11-10 00:48:29 -0800

    finally working

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -1,10 +1,19 @@
 'use strict';
 
+
+function add_direction() {
+	var direc = $("#direction").val();
+	$("#directionslist").append("<li> direc </li>");
+
+}
+
+
 function add_ingredient() {
-	var ingredient = $("#ingredient").val();
-	$("#ingredientlist").append("<li> ingredient <li>" );
+	var ing = $("#ingredient").val();
+	$("#ingredientlist").append("<li> ing </li>" );
 
 	}
 
 $("#readytoadd").click(add_ingredient);
+$("#add_step").click(add_direction)
 
