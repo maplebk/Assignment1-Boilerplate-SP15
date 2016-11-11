a05f33a862b9d9919e5235d590dda6a671b7e824
Michael Garcia <mcg003@ucsd.edu>
2016-11-10 22:12:17 -0800

    fic

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -40,7 +40,7 @@ function add_recipe() {
 							});
 	$.getJSON( "../data.json", function( data ) {
 				console.log(data);
-				var new_recipe = {"name": "+ingredient_name+", "ingredientnum": "+num_of_ingredients+", "preptime": "+prep_time+", "cooktime": "+cook_time+", "diet": "+diet_array+", "course": "+course_type+", "ingredients": "+ingredient_list+", "directions": " + direction_list +", "pic": "https://upload.wikimedia.org/wikipedia/commons/4/49/Gulasch.jpg" };
+				var new_recipe = {"name": +ingredient_name+, "ingredientnum": +num_of_ingredients+, "preptime": +prep_time+, "cooktime": +cook_time+, "diet": +diet_array+, "course": +course_type+, "ingredients": +ingredient_list+, "directions":  + direction_list +, "pic": "https://upload.wikimedia.org/wikipedia/commons/4/49/Gulasch.jpg" };
 				data.recipes.push(new_recipe);
 				});
 }
